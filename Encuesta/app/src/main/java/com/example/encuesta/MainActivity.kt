7c2e35bcf480ed82bf14212a618dd0d254a3b743
DavidMagdaleno <noesis174@gmail.com>
2021-10-17 18:45:56 +0200

    vFinal

--- Encuesta/app/src/main/java/com/example/encuesta/MainActivity.kt
+++ Encuesta/app/src/main/java/com/example/encuesta/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.encuesta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.*
 
@@ -10,13 +11,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    private var lista= arrayListOf<Persona>()
-    private var text:TextView=findViewById(R.id.txtResumen)
+    var lista= arrayListOf<Persona>()
 
     fun validar(view:View){
-
         var sisAux:String=""
-        var espeAux:String=""
+        var nomAux:String=""
+        var anonimo:Switch=findViewById(R.id.swAnonimo)
         var nombre:EditText=findViewById(R.id.etxtNombre)
 
         var sisMac:RadioButton=findViewById(R.id.rbtnMac)
@@ -27,30 +27,56 @@ class MainActivity : AppCompatActivity() {
         var espeASIR:CheckBox=findViewById(R.id.cbASIR)
         var espeDAW:CheckBox=findViewById(R.id.cbDAW)
 
-        //var hora:View=findViewById(R.id.sbHoras)
+        if(sisMac.isChecked){sisAux=sisMac.text.toString()}
+        if(sisWin.isChecked){sisAux=sisWin.text.toString()}
+        if(sisLin.isChecked){sisAux=sisLin.text.toString()}
 
-        if(sisMac.isChecked){sisAux=sisMac.toString()}
-        if(sisWin.isChecked){sisAux=sisWin.toString()}
-        if(sisLin.isChecked){sisAux=sisLin.toString()}
+        if(anonimo.isChecked){
+            nomAux="Anonimo"
+        }else{
+            nomAux=nombre.text.toString()
+        }
 
-        if(espeDAM.isChecked){espeAux=espeAux+","+espeDAM.toString()}
-        if(espeASIR.isChecked){espeAux=espeAux+","+espeASIR.toString()}
-        if(espeDAW.isChecked){espeAux=espeAux+","+espeDAW.toString()}
 
-        var persona=Persona(nombre.toString(),sisAux,espeAux,1)
+        var persona=Persona(nomAux,sisAux)
 
+        if(espeDAM.isChecked){persona.asig(espeDAM.text.toString())}
+        if(espeASIR.isChecked){persona.asig(espeASIR.text.toString())}
+        if(espeDAW.isChecked){persona.asig(espeDAW.text.toString())}
+
+        var hora:SeekBar=findViewById(R.id.sbHoras)
+        var numHoras:TextView=findViewById(R.id.txtNhoras)
+        numHoras.text= hora.progress.toString()
+        persona.setHoras(hora.progress)
+
+        //Log.e("David",persona.toString())---para comprobar la salida
         lista.add(persona)
+
+        anonimo.isChecked=false
+        nombre.setText("")
+        sisMac.isChecked=false
+        sisWin.isChecked=false
+        sisLin.isChecked=false
+        espeDAM.isChecked=false
+        espeDAW.isChecked=false
+        espeASIR.isChecked=false
+        hora.progress=0
+
     }
+
     //fun reiniciar(view: View){}
 
     fun cuantas(view:View){
-        text.text="Hay "+lista.size.toString()+" personas en la lista"
+        var texto:TextView=findViewById(R.id.txtResumen)
+        texto.text=""
+        texto.text="Hay "+lista.size.toString()+" personas en la lista"
     }
     fun resumen(view: View){
-        var textAux:String=""
+        var texto:TextView=findViewById(R.id.txtResumen)
+        texto.text=""
         for(any in lista){
-            textAux=text.toString()
-            text.text= textAux+"\r\n"+any.getNombre()+","+any.getSistema()+","+any.getEspecialidad()+","+any.getHoras()
+               //Log.e("David",any.toString())---para comprobar la salida
+            texto.append(any.toString()+"\r\n")
         }
     }
 
--- Encuesta/app/src/main/java/com/example/encuesta/Persona.kt
+++ Encuesta/app/src/main/java/com/example/encuesta/Persona.kt
@@ -5,36 +5,39 @@ class Persona {
     private var sistema:String=""
     private var especialidad:String=""
     private var horas:Int=0
+    var listaAsig= arrayListOf<String>()
 
     fun getNombre(): String {
         return nombre
     }
-    fun setNombre(n: String) {
-        nombre = n
-    }
     fun getSistema(): String {
         return sistema
-    }
-    fun setSistema(s: String) {
-        sistema = s
     }
     fun getEspecialidad(): String {
         return especialidad
     }
-    fun setEspecialidad(e: String) {
-        especialidad = e
-    }
     fun getHoras(): Int {
         return horas
     }
-    fun setHoras(h: Int) {
-        horas = h
+    fun setHoras(h:Int) {
+         horas=h
     }
     constructor(n:String, s:String,e:String,h:Int){
         this.nombre = n
         this.sistema = s
         this.especialidad = e
         this.horas = h
+    }
+    constructor(n:String, s:String){
+        this.nombre = n
+        this.sistema = s
+    }
+    fun asig(e: String){
+        listaAsig.add(e)
+    }
+
+    override fun toString(): String {
+        return this.getNombre()+","+this.getSistema()+","+this.listaAsig+","+this.getHoras()
     }
 
 }
